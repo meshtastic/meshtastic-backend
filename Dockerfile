1783dfb2a27357553275a0897bdf44b619cb6300
Kevin Hester <kevinh@geeksville.com>
2021-04-09 20:09:24 +0800

    begin docker

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+# per https://cloud.google.com/community/tutorials/kotlin-springboot-container-engine
+# alpine buids are much smaller than other images, not available for anything after jdk8 though
+FROM openjdk:8-alpine
+MAINTAINER meshtastic.org
+VOLUME /tmp
+RUN mkdir /work
+COPY . /work
+WORKDIR /work
+RUN /work/gradlew build
+RUN mv /work/build/libs/*.jar /work/app.jar
+ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/work/app.jar"]
--- docs/building.kt
+++ docs/building.kt
@@ -14,3 +14,11 @@ sudo update-alternatives --config java
 How to configure Intellij for development:
 
 FIXME
+
+## Deployment
+
+This server is built as a Kubernete image which can be deployed in many different containers.  The instructions
+below assume Google Kerbernets Engine.  (I used https://cloud.google.com/community/tutorials/kotlin-springboot-container-engine to make this).
+
+1. Install https://cloud.google.com/sdk/docs/install
+2. "gcloud components install kubectl"
